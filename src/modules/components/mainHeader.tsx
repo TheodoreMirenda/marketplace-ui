c464b949745bbff33df1f5c99a96df79657d73af
TheodoreMirenda <tjmirenda3@gmail.com>
2023-08-07 20:44:56 -0400

    searchbar, some formatting for header

--- src/modules/components/mainHeader.tsx
+++ src/modules/components/mainHeader.tsx
@@ -1,17 +1,27 @@
-import { HStack, Text, Flex } from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { HStack, Text, Flex, Input, InputRightElement, 
+  InputGroup,
+  InputLeftElement
+
+} from '@chakra-ui/react'
 import Link from 'next/link'
+import { SearchBar } from './SearchBar'
 
 
 export default function MainHeader() {
   
   return (
     <>
-    <Flex mt={0} height={'100px'} bgColor = "#aaa">
-
+    <Flex mt={0} height={'50px'} bgColor = {'#2f64ad'} justifyContent={'space-between'}>
+    <Link href="/">
+      <Text fontSize="32"  fontWeight="bold" color="white" ml={10} marginTop={'auto'}>
+        The Fish Store
+      </Text>
+    </Link>
     <HStack>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/login">Sign In</Link>
+    <SearchBar />
+      <Link href="/about">About</Link>
+      <Link href="/login">Sign In</Link>
     </HStack>
     </Flex>
     </>
