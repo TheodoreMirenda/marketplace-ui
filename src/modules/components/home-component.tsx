a6c1a3b94d4320bdc08dfbfc206a5b8637668f13
TJGameDev <98005357+TJGameDev@users.noreply.github.com>
2023-08-24 21:59:05 -0400

    landing page

--- src/modules/components/home-component.tsx
+++ src/modules/components/home-component.tsx
@@ -11,88 +11,79 @@ import {
 import DataCreator from "./create-data";
 import AuthContext from "@src/shared/contexts/auth.context";
 import CartContext from "@src/shared/contexts/cart.context";
+import NextLink from "next/link";
+import { ArrowRightIcon } from "@chakra-ui/icons";
 
 const HomeComponent: FC = () => {
-  // console.log(useContext(AuthContext));
-  // console.log(useContext(CartContext));
-
-  const [fetchUser] = useUserLazyQuery({})
-  const [fetchProduct] = useProductLazyQuery({})
-  const [fetchCategory] = useCategoryLazyQuery({})
-
-  const [login] = useLoginMutation({})
-  const [createUser] = useCreateUserMutation({})
-
-  const handleClick = async () => {
-    const info = await fetchUser({
-      variables: {
-        where: {
-          email: 'tjm3@gmail.com'
-        }
-      }
-    });
-    console.log(info.data?.user?.email);
-  }
-  const handleProduct = async () => {
-    const info = await fetchProduct({
-      variables: {
-        where: {
-          id:1
-        }
-      }
-    });
-    console.log(info.data);
-  }
-  const handleCategory = async () => {
-    const info = await fetchCategory({
-      variables: {
-        where: {
-          id:1
-        }
-      }
-    });
-    console.log(info.data);
-  }
-  const handleCreateUser = async () => {
-    const stuff = await createUser({
-      variables:{
-        data: {
-          email:"use32@gmail.com",
-          password:"password123",
-          username:"user3",
-          firstName:"user3Name",
-          lastName:"user3LastName",
-          avatar:"sdfgsdf",
-          type: Role.User
-        }
-      }
-    }); 
-    console.log(stuff);
-  }
 
   return (
     <>
-        <Flex mt={0} paddingTop={0} justifyContent={'center'} justifyItems={'center'} >
-          <VStack minH={'1000px'}>
-          <Image
+        <Flex mt={0} paddingTop={0} minH={'1920px'} justifyContent={'left'} justifyItems={'left'} >
+        <Image
             position={'absolute'}
             src='/TheFishStore/img/fishTank.jpg'
-            opacity={0.25}
+            opacity={1}
             minH={'450px'}
             zIndex={-1}
             />
+
+          <Flex mt={20} ml={20} w="60%" h="450px" justifyContent={'center'} justifyItems={'center'}
+          bgColor={'fishPalette.darkBlue'}
+          bg={'fishPalette.darkBlue'}
+          opacity={0.95}
+          rounded={'lg'}
+          boxShadow={'lg'}
+          padding={10}
+          >
+            <VStack spacing={2} alignItems={'left'} justifyContent={'left'} justifyItems={'left'}>
             <Text
-              as={'b'}
-              fontSize={'2xl'}
-              alignSelf={'flex-start'}
-              marginBottom={4} 
-              >  </Text>
-              {/* <Button
-                alignSelf={'flex-start'}
-                onClick={createProducts}
-                >create</Button> */}
-        
-          </VStack>
+                fontSize={'md'}
+                color={'fishPalette.gray'}
+                as={'b'}
+                >The Fish Store</Text>
+              <Text
+                fontSize={'3xl'}
+                color={'fishPalette.white'}
+                >Discover a decentralized marketplace where users can buy and sell their aquatic friends :)</Text>
+              <Text
+                fontSize={'lg'}
+                color={'fishPalette.green'}
+                >Shop our wide selection of fish and aquarium accessories</Text>
+                <HStack spacing={8}>
+                
+              <Button mb={10} mt={10} maxW={'200px'} as={NextLink} href="/marketplace" colorScheme={'fishPalette.cyan'} rightIcon={<ArrowRightIcon/>}>Explore</Button>
+              <Button mb={10} mt={10} maxW={'200px'} as={NextLink} href="/login" colorScheme={'fishPalette.cyan'} variant={'outline'}>Login</Button>
+               </HStack>
+                <HStack spacing={8}>
+                  <VStack spacing={0} alignItems={'left'} justifyContent={'left'} justifyItems={'left'}>
+                    <Text
+                      fontSize={'3xl'}
+                      color={'fishPalette.gray'}
+                      as={'b'}
+                      >374</Text>
+                <Text
+                  fontSize={'md'}
+                  color={'fishPalette.gray'}
+                  >Orders Fulfilled</Text>
+
+                  </VStack>
+                  <VStack spacing={0} alignItems={'left'} justifyContent={'left'} justifyItems={'left'}>
+                  <Text
+                      fontSize={'3xl'}
+                      color={'fishPalette.gray'}
+                      as={'b'}
+                      >121</Text>
+                <Text
+                  fontSize={'md'}
+                  color={'fishPalette.gray'}
+                  >Active Listings</Text>
+                  </VStack>
+                </HStack>
+                  
+                  
+            </VStack>
+      
+          </Flex>
       </Flex>
       <DataCreator/>
     </>
